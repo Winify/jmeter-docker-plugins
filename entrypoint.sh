c35363133b75a034fed3d131a7ac9ef0275238cc
Vince Graics <vince.graics@nitrowise.com>
2019-04-04 12:26:03 +0200

    configuring server to test, reportin execution

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,21 +7,37 @@ if [ $# -lt 1 ]; then
         read testfile
 fi
 
-testfile=/opt/tests/$1.jmx
 timestamp=$(date +"%Y%m%d%H%M%S")
+
+mkdir /opt/results/$timestamp
+results=/opt/results/$timestamp
+
+mkdir $results/report
+cp /opt/tests/$1.jmx $results/test.jmx
+
+testfile=$results/test.jmx
 
 if [ -f $testfile ];
     then
+        echo "Start Running JMeter on `date`..."
+
+        echo "Setting Server Hostname set to: $PROTOCOL://$DOMAIN:$PORT"
+        sed -r "s|protocol\">(.*)</|protocol\">$PROTOCOL</|" -i $testfile
+        sed -r "s|domain\">(.*)</|domain\">$DOMAIN</|" -i $testfile
+        sed -r "s|port\">(.*)</|port\">$PORT</|" -i $testfile
+
+        # Setting JVM ARGS
         set -e
         free=`awk '/MemFree/ { print int($2/1024) }' /proc/meminfo`
         usable=$(($free/10*8))
         export JVM_ARGS="-Xms${usable}m -Xmx${usable}m"
+        echo "Setting JVM_ARGS to: ${JVM_ARGS}"
 
-        echo "Start Running JMeter on `date`..."
-        echo "JVM_ARGS=${JVM_ARGS}"
+        echo "Running JMeter Test suite: $1.jmx"
+        jmeter -n -t $testfile -l jmeter_log.jtl -e -o $results/report
 
-        jmeter -n -t $testfile -l jmeter_log.jtl
-        cp jmeter_log.jtl /opt/results/$1_log_$timestamp.jtl
+        echo "Reporting output to: $results/report"
+        cp jmeter_log.jtl $results/$1_log.jtl
         exit 0
 
     else
